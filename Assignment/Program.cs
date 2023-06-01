6d1000c8157df3a3ef7287276ea0a3a2ae41c832
a00269002_AndreaGaviria <a00269002@mycambrian.ca>
2023-06-01 15:45:51 -0400

    Update program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace Assignment
 {
     static class Program
@@ -13,20 +12,23 @@ namespace Assignment
             const int PackMaxItems = 10;
             const float PackMaxVolume = 20;
             const float PackMaxWeight = 30;
-            //Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
-            //PackTester.AddEquipment(pack);
-            
-            List<InventoryItem> items = new List<InventoryItem>();
+            Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            PackTester.AddEquipment(pack);
 
-            items.Add(new Arrow());
+            /*
+            List<InventoryItem> items = new List<InventoryItem>();
+            items.Add(new Sword());
             items.Add(new Rope());
             items.Add(new Water());
             items.Add(new Food());
-
-            foreach(var item in items)
+            items.Add(new Bow());
+            items.Add(new Arrow());
+            foreach (var item in items)
             {
-Console.WriteLine(item.Display());
+                // Massive switch statement if we don't use inheritance/polymorphism
+                System.Console.WriteLine(item.Display());
             }
+            */
         }
     }
 }
